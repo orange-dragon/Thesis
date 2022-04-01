8be346566d7ab094739ed12d88b7c1db21a77862
orange-dragon <mirendafang@163.com>
2022-04-01 23:07:43 +0800

    Change to Latin America

--- try.R
+++ try.R
@@ -2,7 +2,7 @@
 
 
 #########          
-#########   1  Eastern Mediterranean ---- response index
+#########   1  response index
 #########
 
 library(dslabs)
@@ -16,45 +16,63 @@ data<-read.csv("covid-stringency-index.csv")
 data$Entity
 colnames(data)[4] <- 'Stringency_Index'  
 
-East_Medit <- c("Iran","Saudi Arabia","Pakistan","Iraq","Qatar","Egypt") 
-data1 =data[which(data $ Entity %in% East_Medit),] 
+Latin <- c("Brazil","Argentina","Colombia","Mexico","Peru","Chile") 
+data1 =data[which(data $ Entity %in% Latin),] 
 rownames(data1) <- NULL  
-write.csv(data1, file="East_Medit.csv") 
+write.csv(data1, file="Latin.csv") 
 
-East_Medit1 <-read.csv("East_Medit.csv",header=T) 
+Latin1 <-read.csv("Latin.csv",header=T) 
 
-#######  1.1 Eastern Mediterranean --- boxplot and Violin Plot
+#######  1.1 boxplot and Violin Plot
 
 ## Violin Plot 
-p1 = ggplot(East_Medit1,aes(x =Entity, y = Stringency_Index,fill = Entity)) +geom_violin()
+p1 = ggplot(Latin1,aes(x =Entity, y = Stringency_Index,fill = Entity)) +geom_violin()
 print(p1)
 
 ## Boxplot 
-p2 = ggplot(East_Medit1,aes(x = Entity, y = Stringency_Index,fill = Entity)) +geom_boxplot()+
+p2 = ggplot(Latin1,aes(x = Entity, y = Stringency_Index,fill = Entity)) +geom_boxplot()+
   theme(axis.title.x  = element_text(face = 'italic'),
         axis.text.x =  element_text(angle = 45 , vjust = 0.5))
 print(p2)
 
 ## Density Plot                            
-p3 = ggplot(East_Medit1,aes(Stringency_Index,col=Entity)) +geom_density() + 
+p3 = ggplot(Latin1,aes(Stringency_Index,col=Entity)) +geom_density() + 
   facet_wrap(~Entity,nrow = 2)  #nrow是显示行数
 print(p3)
 
-####### 1.2  Eastern Mediterranean 6 --- Nonpharmaceutical Intervention Actions
+####### 1.2  Nonpharmaceutical Intervention Actions
 
-### "Iran","Saudi Arabia","Pakistan","Iraq","Qatar","Egypt"   
+### "Brazil","Argentina","Colombia","Mexico","Peru","Chile"  
 
-Iran =data1[which(data1 $ Entity=="Iran"),] 
-rownames(Iran) <- NULL  
-colnames(Iran)[4] <- 'Iran_Index' 
-Iran = Iran %>% select(Day,Iran_Index) 
+Brazil =data1[which(data1 $ Entity=="Brazil"),] 
+rownames(Brazil) <- NULL  
+colnames(Brazil)[4] <- 'Brazil_Index' 
+Brazil = Brazil %>% select(Day,Brazil_Index) 
 
-Egypt =data1[which(data1 $ Entity=="Egypt"),] 
-rownames(Egypt) <- NULL  
-colnames(Egypt)[4] <- 'Egypt_Index' 
-Egypt = Egypt %>% select(Day,Egypt_Index) 
+Argentina =data1[which(data1 $ Entity=="Argentina"),] 
+rownames(Argentina) <- NULL  
+colnames(Argentina)[4] <- 'Argentina_Index' 
+Argentina = Argentina %>% select(Day,Argentina_Index) 
 
-###Adding the remaining countries
+Colombia =data1[which(data1 $ Entity=="Colombia"),] 
+rownames(Colombia) <- NULL  
+colnames(Colombia)[4] <- 'Colombia_Index' 
+Colombia = Colombia %>% select(Day,Colombia_Index) 
+
+Mexico =data1[which(data1 $ Entity=="Mexico"),] 
+rownames(Mexico) <- NULL  
+colnames(Mexico)[4] <- 'Mexico_Index' 
+Mexico = Mexico %>% select(Day,Mexico_Index) 
+
+Peru =data1[which(data1 $ Entity=="Peru"),] 
+rownames(Peru) <- NULL  
+colnames(Peru)[4] <- 'Peru_Index' 
+Peru = Peru %>% select(Day,Peru_Index) 
+
+Chile =data1[which(data1 $ Entity=="Chile"),] 
+rownames(Chile) <- NULL  
+colnames(Chile)[4] <- 'Chile_Index' 
+Chile = Chile %>% select(Day,Chile_Index) 
 
 
 # Define a function-multimerge
@@ -68,17 +86,21 @@ multimerge<-function(dat=list(),...){
   return(mergedat)
 }
 
-East_Medit6<- multimerge(list(Iran, Egypt))             
+Latin6<- multimerge(list(Brazil,Argentina,Colombia,Mexico,Peru,Chile))             
 
 library(lubridate)
-East_Medit6$Day <- lubridate::ymd(East_Medit6$Day)
+Latin6$Day <- lubridate::ymd(Latin6$Day)
 
-write.csv(East_Medit6, file="EM_Index.csv")  
+write.csv(Latin6, file="Latin_Index.csv")  
 
 library(ggplot2)
 
-vd = rbind(data.frame(v=East_Medit6$Day, y=East_Medit6$Iran_Index, type=as.factor(1)), 
-           data.frame(v=East_Medit6$Day, y=East_Medit6$Egypt_Index, type=as.factor(2)))
+vd = rbind(data.frame(v=Latin6$Day, y=Latin6$Brazil_Index, type=as.factor(1)), 
+           data.frame(v=Latin6$Day, y=Latin6$Argentinagypt_Index, type=as.factor(2)),
+           data.frame(v=Latin6$Day, y=Latin6$Colombia_Index, type=as.factor(3)),
+           data.frame(v=Latin6$Day, y=Latin6$Mexico_Index, type=as.factor(4)),
+           data.frame(v=Latin6$Day, y=Latin6$Peru_Index, type=as.factor(5)),
+           data.frame(v=Latin6$Day, y=Latin6$Chile_Index, type=as.factor(6)))
 
 ggplot(vd,aes(x=v,y=y,shape=type,color=type,group=type))+geom_point()+labs(title="Covid-19 Response Stringency Index")+xlab("Date")+ylab("Response Stringency Index")+scale_shape_manual(values=c(1,4,5,7,9,11))+scale_color_manual(values=c(1:6))+theme(plot.title=element_text(hjust=0.5))+geom_line()
 dev.off()
@@ -89,7 +111,7 @@ dev.off()
 
 
 #########          
-#########   2  Eastern Mediterranean --- Confirmed cases per million people
+#########   2  Confirmed cases per million people
 #########
 
 
@@ -98,62 +120,61 @@ data<-read.csv("owid-covid-data.csv")
 data$location 
 colnames(data)[11] <- 'Cases_per_million'  
 
-East_Medit <- c("Iran","Saudi Arabia","Pakistan","Iraq","Qatar","Egypt") 
+Latin <- c("Brazil","Argentina","Colombia","Mexico","Peru","Chile") 
 
-data1 =data[which(data$location %in% East_Medit),]  
+data1 =data[which(data$location %in% Latin),]  
 rownames(data1) <- NULL  
-write.csv(data1, file="EM_permillon.csv") 
-EM_permillon1= data1
+write.csv(data1, file="Latin_permillon.csv") 
+Latin_permillon1= data1
 
-#######  2.1 Eastern Mediterranean - Violin Plot and Boxplot
+#######  2.1 Violin Plot and Boxplot
 
 ## Violin Plot 
-p1 = ggplot(EM_permillon1,aes(x =location, y = Cases_per_million,fill = location)) +geom_violin()
+p1 = ggplot(Latin_permillon1,aes(x =location, y = Cases_per_million,fill = location)) +geom_violin()
 print(p1)
 
 ## Boxplot 
-p2 = ggplot(EM_permillon1,aes(x = location, y = Cases_per_million,fill =location)) +geom_boxplot()+
+p2 = ggplot(Latin_permillon1,aes(x = location, y = Cases_per_million,fill =location)) +geom_boxplot()+
   theme(axis.title.x  = element_text(face = 'italic'),
         axis.text.x =  element_text(angle = 45 , vjust = 0.5))
 print(p2)
 
 ## Density Plot 
-p3 = ggplot(EM_permillon1,aes(Cases_per_million,col=location)) +geom_density() + 
+p3 = ggplot(Latin_permillon1,aes(Cases_per_million,col=location)) +geom_density() + 
   facet_wrap(~location,nrow = 2)  
 
-#######  2.2  Eastern Mediterranean - The line chart
+#######  2.2  The line chart
 
 library(dplyr)
 
-### "Iran","Saudi Arabia","Pakistan","Iraq","Qatar","Egypt" 
-
+### "Brazil","Argentina","Colombia","Mexico","Peru","Chile" 
 data<-read.csv("owid-covid-data.csv")   
 colnames(data)[3] <- 'Entity'   
 colnames(data)[4] <- 'Date'   
 
-Iran = filter(data,data$Entity=="Iran")  
-colnames(Iran)[11] <- 'Iran_Index'       
-Iran = Iran %>% select(Date,Iran_Index)   
+Brazil = filter(data,data$Entity=="Brazil")  
+colnames(Brazil)[11] <- 'Brazil_Index'       
+Brazil = Brazil %>% select(Date,Brazil_Index)   
 
-Saudi_Arabia = filter(data,data$Entity=="Saudi Arabia")
-colnames(Saudi_Arabia)[11] <- 'Saudi_Arabia_index' 
-Saudi_Arabia = Saudi_Arabia %>% select(Date,Saudi_Arabia_index) 
+Argentina = filter(data,data$Entity=="Argentina")
+colnames(Argentina)[11] <- 'Argentina_index' 
+Argentina = Argentina %>% select(Date,Argentina_index) 
 
-Pakistan = filter(data,data$Entity=="Pakistan")
-colnames(Pakistan)[11] <- 'Pakistan_index' 
-Pakistan = Pakistan %>% select(Date,Pakistan_index) 
+Colombia = filter(data,data$Entity=="Colombia")
+colnames(Colombia)[11] <- 'Colombia_index' 
+Colombia = Colombia %>% select(Date,Colombia_index) 
 
-Iraq = filter(data,data$Entity=="Iraq")
-colnames(Iraq)[11] <- 'Iraq_Index' 
-Iraq = Iraq %>% select(Date,Iraq_Index) 
+Mexico = filter(data,data$Entity=="Mexico")
+colnames(Mexico)[11] <- 'Mexico_Index' 
+Mexico = Mexico %>% select(Date,Mexico_Index) 
 
-Qatar = filter(data,data$Entity=="Qatar")
-colnames(Qatar)[11] <- 'Qatar_Index' 
-Qatar = Qatar %>% select(Date,Qatar_Index) 
+Peru = filter(data,data$Entity=="Peru")
+colnames(Peru)[11] <- 'Peru_Index' 
+Peru = Peru %>% select(Date,Peru_Index) 
 
-Egypt = filter(data,data$Entity=="Egypt")
-colnames(Egypt)[11] <- 'Egypt_Index' 
-Egypt = Egypt %>% select(Date,Egypt_Index) 
+Chile = filter(data,data$Entity=="Chile")
+colnames(Chile)[11] <- 'Chile_Index' 
+Chile = Chile %>% select(Date,Chile_Index) 
 
 
 # multimerge
@@ -167,21 +188,21 @@ multimerge<-function(dat=list(),...){
   return(mergedat)
 }
 
-EM_permillon6<- multimerge(list(Iran,Saudi_Arabia, Pakistan, Iraq, Qatar, Egypt))             
-write.csv(EM_permillon6, file="EM_permillon6.csv") 
+EM_permillon6<- multimerge(list(Brazil,Argentina,Colombia,Mexico,Peru,Chile))             
+write.csv(Latin_permillon6, file="Latin_permillon6.csv") 
 
 library(lubridate)
-East_Medit6$Date <- lubridate::dmy(East_Medit6$Date)
+Latin6$Date <- lubridate::dmy(Latin6$Date)
 
-East_Medit6 <- East_Medit6[,-1]  
+Latin6 <- Latin6[,-1]  
 
 library(ggplot2)
-vd = rbind(data.frame(v=East_Medit6$Date, y=East_Medit6$Iran_Index, type=as.factor(1)), 
-           data.frame(v=East_Medit6$Date, y=East_Medit6$Saudi_Arabia_index, type=as.factor(2)),
-           data.frame(v=East_Medit6$Date, y=East_Medit6$Pakistan_index, type=as.factor(3)), 
-           data.frame(v=East_Medit6$Date, y=East_Medit6$Iraq_Index, type=as.factor(4)), 
-           data.frame(v=East_Medit6$Date, y=East_Medit6$Qatar_Index, type=as.factor(5)),
-           data.frame(v=East_Medit6$Date, y=East_Medit6$Egypt_Index, type=as.factor(6)))
+vd = rbind(data.frame(v=Latin6$Date, y=Latin6$Brazil_Index, type=as.factor(1)), 
+           data.frame(v=Latin6$Date, y=Latin6$Argentina_index, type=as.factor(2)),
+           data.frame(v=Latin6$Date, y=Latin6$Colombia_index, type=as.factor(3)), 
+           data.frame(v=Latin6$Date, y=Latin6$Mexico_Index, type=as.factor(4)), 
+           data.frame(v=Latin6$Date, y=Latin6$Peru_Index, type=as.factor(5)),
+           data.frame(v=Latin6$Date, y=Latin6$Chile_Index, type=as.factor(6)))
 
 ggplot(vd,aes(x=v,y=y,shape=type,color=type,group=type))+geom_point()+labs(title="Total confirmed cases of COVID-19 per million people ")+xlab("Date")+ylab("Total confirmed cases per million")+scale_shape_manual(values=c(1,4,5,7,9,11))+scale_color_manual(values=c(1:6))+theme(plot.title=element_text(hjust=0.5))+geom_line()
 
@@ -191,7 +212,7 @@ dev.off()
 
 
 #########          
-#########   3  Eastern Mediterranean --- response index and the number of confirmed   #########
+#########   3  response index and the number of confirmed   #########
 
 library(dslabs)
 library(dplyr)
@@ -200,62 +221,62 @@ library(ggplot2)
 
 data<-read.csv("owid-covid-data.csv")    
 
-East_Medit <- c("Iran","Saudi Arabia","Pakistan","Iraq","Qatar","Egypt") 
+Latin <- c("Brazil","Argentina","Colombia","Mexico","Peru","Chile") 
 
-data1 =data[which(data$location %in% East_Medit),] 
+data1 =data[which(data$location %in% Latin),] 
 rownames(data1) <- NULL 
 data1 = data1 %>% select(location,date,total_cases) 
 colnames(data1)[1] <- 'Entity' 
 colnames(data1)[2] <- 'Date' 
-write.csv(data1, file="EM_totalcase.csv") 
-EM_totalcase1 <- data1
+write.csv(data1, file="Latin_totalcase.csv") 
+Latin_totalcase1 <- data1
 
-#######  3.1 Eastern Mediterranean --- Violin Plot and Boxplot for Total number of confirmed cases
+#######  3.1 Violin Plot and Boxplot for Total number of confirmed cases
 
 ## Violin Plot 
-p1 = ggplot(EM_totalcase1,aes(x =Entity, y = total_cases,fill = Entity)) +geom_violin()
+p1 = ggplot(Latin_totalcase1,aes(x =Entity, y = total_cases,fill = Entity)) +geom_violin()
 print(p1)
 
 ## Boxplot 
-p2 = ggplot(EM_totalcase1,aes(x = Entity, y = total_cases,fill = Entity)) +geom_boxplot()+
+p2 = ggplot(Latin_totalcase1,aes(x = Entity, y = total_cases,fill = Entity)) +geom_boxplot()+
   theme(axis.title.x  = element_text(face = 'italic'),
         axis.text.x =  element_text(angle = 45 , vjust = 0.5))
 print(p2)
 
 ## Density Plot                            
-p3 = ggplot(EM_totalcase1,aes(total_cases,col=Entity)) +geom_density() + 
+p3 = ggplot(Latin_totalcase1,aes(total_cases,col=Entity)) +geom_density() + 
   facet_wrap(~Entity,nrow = 2) 
 print(p3)
 
-####### 1.2  Eastern Mediterranean --- Total number of confirmed cases
+####### 3.2  Total number of confirmed cases
 
 library(dplyr)
-data <- EM_totalcase1
-### "Iran","Saudi Arabia","Pakistan","Iraq","Qatar","Egypt"   
+data <- Latin_totalcase1
+### "Brazil","Argentina","Colombia","Mexico","Peru","Chile"
 
-Iran = filter(data,data$Entity=="Iran")
-colnames(Iran)[4] <- 'Iran_total' 
-Iran = Iran %>% select(Date,Iran_total) 
+Brazil = filter(data,data$Entity=="Brazil")
+colnames(Brazil)[4] <- 'Brazil_total' 
+Brazil = Brazil %>% select(Date,Brazil_total) 
 
-Saudi_Arabia = filter(data,data$Entity=="Saudi Arabia")
-colnames(Saudi_Arabia)[4] <- 'Saudi_Arabia_total' 
-Saudi_Arabia = Saudi_Arabia %>% select(Date,Saudi_Arabia_total) 
+Argentina = filter(data,data$Entity=="Argentina")
+colnames(Argentina)[4] <- 'Argentina_total' 
+Argentina = Argentina %>% select(Date,Argentina_total) 
 
-Pakistan = filter(data,data$Entity=="Pakistan")
-colnames(Pakistan)[4] <- 'Pakistan_total' 
-Pakistan = Pakistan %>% select(Date,Pakistan_total) 
+Colombia = filter(data,data$Entity=="Colombia")
+colnames(Colombia)[4] <- 'Colombia_total' 
+Colombia = Colombia %>% select(Date,Colombia_total) 
 
-Iraq = filter(data,data$Entity=="Iraq")
-colnames(Iraq)[4] <- 'Iraq_total' 
-Iraq = Iraq %>% select(Date,Iraq_total) 
+Mexico = filter(data,data$Entity=="Mexico")
+colnames(Mexico)[4] <- 'Mexico_total' 
+Mexico = Mexico %>% select(Date,Mexico_total) 
 
-Qatar = filter(data,data$Entity=="Qatar")
-colnames(Qatar)[4] <- 'Qatar_total' 
-Qatar = Qatar %>% select(Date,Qatar_total) 
+Peru = filter(data,data$Entity=="Peru")
+colnames(Peru)[4] <- 'Peru_total' 
+Peru = Peru %>% select(Date,Peru_total) 
 
-Egypt = filter(data,data$Entity=="Egypt")
-colnames(Egypt)[4] <- 'Egypt_total' 
-Egypt = Egypt %>% select(Date,Egypt_total) 
+Chile = filter(data,data$Entity=="Chile")
+colnames(Chile)[4] <- 'Chile_total' 
+Chile = Chile %>% select(Date,Chile_total) 
 
 # multimerge
 multimerge<-function(dat=list(),...){
@@ -268,37 +289,37 @@ multimerge<-function(dat=list(),...){
   return(mergedat)
 }
 
-EM_totalcase6<- multimerge(list(Iran,Saudi_Arabia, Pakistan, Iraq, Qatar, Egypt))             
-write.csv(EM_totalcase6, file="EM_totalcase6.csv") 
+Latin_totalcase6<- multimerge(list(Brazil,Argentina,Colombia,Mexico,Peru,Chile))             
+write.csv(Latin_totalcase6, file="Latin_totalcase6.csv") 
 
 
 ####### 3.3   relationship between government response index and the number of confirmed cases
 
 
-EM_Index <- read.csv("EM_Index.csv")
+Latin_Index <- read.csv("Latin_Index.csv")
 ##Remove the first column
-EM_Index<- EM_Index[,-1]
-EM_Index_mean <- apply(EM_Index[1:419,2:7], 2, mean,na.rm=T)  
-EM_Index_mean   
+Latin_Index<- Latin_Index[,-1]
+Latin_Index_mean <- apply(Latin_Index[1:419,2:7], 2, mean,na.rm=T)  
+Latin_Index_mean   
 
 #Iran_Index Saudi_Arabia_index     Pakistan_index         Iraq_Index        Qatar_Index        Egypt_Index 
 #51.80103           55.24105           54.81881           62.92697           60.56647           54.73883 
 
-EM_Index_Median <- apply(EM_Index[1:419,2:7], 2, median,na.rm=T)  
-EM_Index_Median
+Latin_Index_Median <- apply(Latin_Index[1:419,2:7], 2, median,na.rm=T)  
+Latin_Index_Median
 
 #Iran_Index Saudi_Arabia_index     Pakistan_index         Iraq_Index        Qatar_Index        Egypt_Index 
 #58.80              57.41              58.80              61.11              64.81              62.96 
 
-class(EM_Index_Median)
+class(Latin_Index_Median)
 
-Index_mean = matrix(EM_Index_mean)                       
-Index_Median = matrix(EM_Index_Median)                    
+Index_mean = matrix(Latin_Index_mean)                       
+Index_Median = matrix(Latin_Index_Median)                    
 
-Entity = c("Iran","Saudi Arabia","Pakistan","Iraq","Qatar","Egypt")
+Entity = c("Brazil","Argentina","Colombia","Mexico","Peru","Chile")
 
-EM_Total = EM_totalcase6
-Total_cases = EM_Total[392,]
+Latin_Total = Latin_totalcase6
+Total_cases = Latin_Total[392,]
 Total_cases = Total_cases[,-1] 
 class(Total_cases)
 Total_cases
@@ -308,7 +329,7 @@ rownames(Total_cases) <- NULL
 Total_cases
 Total_cases <- t(Total_cases)
 
-Casepermillion = EM_permillon6 
+Casepermillion = Latin_permillon6 
 Casepermillion = Casepermillion[392,]
 Casepermillion = Casepermillion[,-1] 
 class(Casepermillion)
@@ -320,7 +341,7 @@ Casepermillion
 Casepermillion<- t(Casepermillion)
 Casepermillion
 
-lastday_index = EM_Index[418,]
+lastday_index = Latin_Index[418,]
 lastday_index = lastday_index[,-1] 
 class(lastday_index)
 lastday_index
@@ -337,7 +358,7 @@ mix= data.frame(Entity,Total_cases,Casepermillion,lastday_index,Index_mean,Index
 
 class(mix)
 
-write.csv(mix, file=" EM_Index and Case.csv") 
+write.csv(mix, file=" Latin_Index and Case.csv") 
 
 #index and total number of confirmed cases
 ggplot(mix,aes(x=mix$Total_cases,y=mix$lastday_index))+geom_point(stat="identity",fill="steelblue",colour="steelblue",width = 1.8,size=8.5)+labs(title="Relationship between Total confirmed cases of COVID-19 and Response Index")+xlab("Total confirmed cases of COVID-19")+ylab("Response Index")+theme(plot.title=element_text(hjust=0.5))+geom_text(aes(label = mix$Entity, vjust = -1.2, hjust = 0.0), show.legend = TRUE)
